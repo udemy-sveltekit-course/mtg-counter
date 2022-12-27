1b5ba5792e18240a30b55b9234ceabe829354058
phptuts <glaserpower@gmail.com>
2022-12-27 13:18:41 -0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Team from './Team.svelte';
+  let redScore = 20;
+  let blueScore = 20;
+  $: redWon = blueScore === 0;
+  $: blueWon = redScore === 0;
+  $: gameOver = redWon || blueWon;
+  $: btnRestText = gameOver ? 'New Game' : 'Reset Game';
+  function resetGame() {
+    redScore = 20;
+    blueScore = 20;
+  }
+</script>
+
+<div class="row mb-5">
+  <div class="col">
+    <h1 class="text-center">MTG Counter</h1>
+  </div>
+</div>
+<div class="row">
+  <Team {gameOver} teamName="Red" bind:points={redScore} />
+  <Team {gameOver} teamName="Blue" bind:points={blueScore} />
+</div>
+{#if gameOver}
+  <div class="row mt-3 mb-3">
+    <div class="col">
+      {#if redWon}
+        <h2 class="text-center text-danger">Red Won</h2>
+      {:else}
+        <h2 class="text-center text-primary">Blue Won</h2>
+      {/if}
+    </div>
+  </div>
+{/if}
+<div class="row mt-3">
+  <div class="col">
+    <button
+      on:click={resetGame}
+      class:btn-success={gameOver}
+      class:btn-warning={!gameOver}
+      class="btn w-100">{btnRestText}</button
+    >
+  </div>
+</div>
---
+++ src/Team.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let teamName;
+  export let points;
+  export let gameOver;
+  function add() {
+    points += 1;
+  }
+  function minus() {
+    points -= 1;
+  }
+</script>
+
+<div class="col" style="border: dashed 5px {teamName.toLowerCase()}">
+  <h2
+    class:text-primary={teamName === 'Blue'}
+    class:text-danger={teamName === 'Red'}
+  >
+    {teamName}: {points}
+  </h2>
+  <button disabled={gameOver} on:click={minus} class="btn btn-danger">-</button>
+  <button disabled={gameOver} on:click={add} class="btn btn-success">+</button>
+</div>
+
+<style>
+  div {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
